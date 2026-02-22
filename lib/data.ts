4be092b27c40c8e485c2192634ec9311a2037d4d
LastHolmz <databaserepos@gmail.com>
2026-02-22 15:53:05 +0200

    prod

--- lib/data.ts
+++ lib/data.ts
@@ -1,14 +1,15 @@
 export const data = {
   header: {
     companyName: "لاستيراد الآلات والمعدات الثقيلة",
-    tagline: "شركة الافق الصناعي",
+    tagline: "شركة الدراع الاساسي",
     phone: "0940571111",
   },
   commonInfo: {
-    companyName: "شركة الافق الصناعي",
-    heroDesc: "شركة الافق الصناعي لاستيراد الآلات والمعدات الثقيلة وقطع غيارها",
+    companyName: "شركة الدراع الاساسي",
+    heroDesc:
+      "شركة الدراع الاساسي لاستيراد الآلات والمعدات الثقيلة وقطع غيارها",
     phone: "0940571111",
-    domain: "https://alofuq-alsinaei.com.ly",
+    domain: "https://aldhiraa-alasasi.com.ly",
   },
   legal: [
     { no: "01", label: "رقم القيد", value: "_" },
@@ -18,19 +19,19 @@ export const data = {
   ],
   contact: {
     phones: ["0940571111", "0944041633"],
-    email: "info@alofuq-alsinaei.com.ly",
+    email: "info@aldhiraa-alasasi.com.ly",
     address: "جوددائم /الزاوية /ليبيا",
     mapEmbedUrl:
       "https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3354.176408256291!2d12.797122975663243!3d32.787581473659394!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMzLCsDQ3JzE1LjMiTiAxMsKwNDcnNTguOSJF!5e0!3m2!1sen!2sly!4v1770701749424!5m2!1sen!2sly",
     mapLink: "https://maps.app.goo.gl/uTmPeXxiYvZnahhc9",
   },
   footer: {
-    email: "info@alofuq-alsinaei.com.ly",
+    email: "info@aldhiraa-alasasi.com.ly",
     phones: ["0940571111", "0944041633"],
     address: "جوددائم /الزاوية /ليبيا",
-    brand: "شركة الافق الصناعي لاستيراد الآلات والمعدات الثقيلة",
+    brand: "شركة الدراع الاساسي لاستيراد الآلات والمعدات الثقيلة",
     designerLabel: "تم التطوير بواسطة منفذ",
     designerHref: "https://mnfd.ly",
-    companyName: "الافق الصناعي",
+    companyName: "الدراع الاساسي",
   },
 };
